4c4b58048dc0477e5e8bd7a921570eb79b4d723d
dualism98 <58901926+dualism98@users.noreply.github.com>
2020-05-25 00:00:09 +0700

    Update ListElement.js

--- src/ListElement.js
+++ src/ListElement.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
-export default function ListElement({text, id, onChange, check}){
+export default function ListElement({text, id, onChange, onClick, check}){
 
     const classes = ['Listtext']
-
+    var checker = false
     if(check){
         classes.push('done')
+        checker = true
     }
 
     return(
         <li>
             <span>
-                <input type="checkbox" className="Checkbox" onChange={() => onChange(id)}/>
+                <input type="checkbox" className={'Checkbox'} checked={checker} onChange={() => onChange(id)}/>
             </span>
             <p className={classes.join(' ')}>{text}</p>
+            <input type="button" className="Delete" onClick={() => onClick(id)} value='&times;' />
         </li>
     )
 }
